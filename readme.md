4851b0c8d5e10fa7be1c26b227aae57bf20ecdcc
gluck3 <pankaj@vanamvanguard.com>
2024-07-08 17:02:05 +0200

    made starfm pip installable

---
+++ readme.md
@@ -0,0 +1,9 @@
+# STARFM for Python (starfm4py)
+
+Installable version of STARFM algorithm for satellite image fusion implemented in python ([original repo](https://github.com/nmileva/starfm4py)).
+
+## Installation
+
+```python
+pip install --upgrade git+https://github.com/gluck3/starfm
+```
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+with open('requirements.txt') as f:
+    install_req = [req.strip() for req in f.read().split('\r\n')]
+install_req = [req for req in install_req if req and req[0] != '#']
+
+with open("readme.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+     name='starfm',
+     version='0.0.1',
+     description='STARFM for Python',
+     long_description=long_description,
+     long_description_content_type='text/markdown',
+     url='https://github.com/gluck3/starfm.git',
+     license='MIT',
+     packages=find_packages(),
+     py_modules=['starfm'],
+     install_requires=install_req,
+     setup_requires=['setuptools'],
+)
